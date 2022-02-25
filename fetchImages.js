8000cdf3be1e0b7b01a5566b62efede60f775d38
See-Bee9 <colin_bird2000@yahoo.com>
2022-02-24 20:34:58 -0500

    Added tools to fetch and cache images.

---
+++ fetchImages.js
@@ -0,0 +1,46 @@
+// import {writeFileAsync} from 'fs';
+// import {readFile} from 'fs/promises';
+
+import { readFile, writeFile, createWriteStream } from 'fs';
+import { getLinkPreview } from "link-preview-js";
+import path from 'path';
+import * as client from 'https';
+
+const fileName = './src/json/main.json';
+const fileBase = './src/img/';
+
+const file = [];
+
+await readFile(fileName, 'utf8', async (err, data) => {
+    const parsed = JSON.parse(data);
+    file.push(...parsed);
+    console.log(file);
+    await getImages();
+});
+
+const getImages = async () => {
+    for (let i = 0; i < file.length; i++) {
+        const location = file[i];
+        await getLinkPreview(location.url)
+            .then(async d => {
+                if (d.images && d.images.length > 0) {
+                    const bestGuess = d.images[0];
+                    const baseName  = path.basename(bestGuess);
+
+
+                    const filename = `${fileBase}${baseName}`;
+                    client.get(bestGuess, (response) => {
+                        console.log(filename);
+                        response.pipe(createWriteStream(filename))
+                    })
+                    console.log(baseName);
+                    console.log(bestGuess);
+                    location.image = filename;
+                }
+            });
+    }
+    writeFile(fileName, JSON.stringify(file), err => {
+    })
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 (function () {
-
     const generate = (title, desciption, url) => {
         return {
             'title'      : title,
@@ -7,27 +6,28 @@
             'url'        : url
         };
     }
-    const data     = [
-        {
-            "title"      : "Barefoot Bernies's",
-            "url"        : "https://www.barefootbernies.com/",
-            "description": "Laid back bar and grill",
-        },
-        generate('Owens', 'Fine dining', 'https://owensrestaurant.com/')
-    ];
-    const content  = document.querySelector('#content');
-    for (const location of data) {
-        if ('content' in document.createElement('template')) console.log('We have a template');
-        const template = document.querySelector('#content-template');
-        const clone    = template.content.cloneNode(true);
 
-        const title     = clone.querySelector('.title')
-        title.innerHTML = location.title;
-        title.setAttribute('href', location.url);
+    const content = document.querySelector('#content');
+    fetch("/json/main.json")
+        .then(response => response.json())
+        .then(data => processData(data));
 
-        clone.querySelector('.description')
-            .innerHTML = location.description;
-        content.appendChild(clone);
+    const processData = (data) => {
+        for (const location of data) {
+            if ('content' in document.createElement('template')) console.log('We have a template');
+            const template = document.querySelector('#content-template');
+            const clone    = template.content.cloneNode(true);
 
+            const title     = clone.querySelector('.title')
+            title.innerHTML = location.title;
+            title.setAttribute('href', location.url);
+
+            clone.querySelector('.description')
+                .innerHTML = location.description;
+
+
+            content.appendChild(clone);
+
+        }
     }
 })()
