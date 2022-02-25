2025e4063190aad0d9af500ef56d820da9fef0a4
See-Bee9 <colin_bird2000@yahoo.com>
2022-02-25 18:27:02 -0500

    Added tab navigation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
 (function () {
 
 
-    document.getElementById('hamburger')
-            .addEventListener('click', () => {
-                document.getElementById("navbarBasicExample")
-                        .classList.toggle('is-active');
-            })
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
     document.querySelectorAll('.navbar-item')
             .forEach(item => item.addEventListener('click', event => {
                 const target = event.target.id.split('-')[1];
